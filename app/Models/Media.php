3ebae6046af0f6fdcbb7e263ccd21ae43c48f50e
PETER-p220 <mushyp420@gmail.com>
2026-01-30 19:53:02 +0300

    comm

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -30,7 +30,7 @@ class Media extends Model
     public function getUrlAttribute()
     {
         // Get base URL from config
-        $baseUrl = config('app.url', 'http://localhost:8000');
+        $baseUrl = config('app.url', 'http://31.97.176.48:8081');
         
         // Parse the URL to check if port is missing
         $parsedUrl = parse_url($baseUrl);
@@ -42,7 +42,7 @@ class Media extends Model
             $scheme = $parsedUrl['scheme'] ?? 'http';
             $host = $parsedUrl['host'];
             $path = $parsedUrl['path'] ?? '';
-            $baseUrl = "{$scheme}://{$host}:8000{$path}";
+            $baseUrl = "{$scheme}://{$host}:8081{$path}";
         }
         
         // Construct URL manually to ensure correct format
